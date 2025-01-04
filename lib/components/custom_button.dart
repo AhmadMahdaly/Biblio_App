c454059844f2e9f771b598cdccc8a2a9e5437c00
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-01-04 13:54:17 +0200

    Enhance CustomButton and OnboardScreen UI
    - Added `onTap` callback to `CustomButton` and `CustomBorderBotton` for interactive behavior.
    - Updated `OnboardScreen` with a detailed UI including an image, text, and buttons.
    - Added new font assets for better typography support.
    - Improved layout and styling for better user experience.

--- lib/components/custom_button.dart
+++ lib/components/custom_button.dart
@@ -6,49 +6,59 @@ class CustomButton extends StatelessWidget {
   const CustomButton({
     required this.text,
     this.isActive = true,
+    this.onTap,
     super.key,
   });
 
   final String text;
   final bool isActive;
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
     return isActive
-        ? Container(
-            alignment: Alignment.center,
-            width: double.infinity,
-            height: 56.sp,
-            decoration: ShapeDecoration(
-              color: kMainColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12.sp),
+        ? InkWell(
+            onTap: onTap,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 16.sp),
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: 56.sp,
+              decoration: ShapeDecoration(
+                color: kMainColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.sp),
+                ),
               ),
-            ),
-            child: Text(
-              text,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16.sp,
-                fontWeight: FontWeight.w800,
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w800,
+                ),
               ),
             ),
           )
-        : Container(
-            alignment: Alignment.center,
-            width: double.infinity,
-            height: 56.sp,
-            decoration: ShapeDecoration(
-              color: kDisableButtonColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12.sp),
+        : InkWell(
+            onTap: onTap,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 16.sp),
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: 56.sp,
+              decoration: ShapeDecoration(
+                color: kDisableButtonColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.sp),
+                ),
               ),
-            ),
-            child: Text(
-              text,
-              style: TextStyle(
-                color: kTextShadowColor,
-                fontSize: 16.sp,
-                fontWeight: FontWeight.w800,
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: kTextShadowColor,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w800,
+                ),
               ),
             ),
           );
@@ -57,32 +67,37 @@ class CustomButton extends StatelessWidget {
 
 class CustomBorderBotton extends StatelessWidget {
   const CustomBorderBotton({
-    required this.borderColor,
     required this.text,
+    this.borderColor = kMainColor,
     super.key,
+    this.onTap,
   });
 
   final Color borderColor;
-
+  final void Function()? onTap;
   final String text;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      width: double.infinity,
-      height: 56.sp,
-      decoration: ShapeDecoration(
-        color: kMainColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12.sp),
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 16.sp),
+        alignment: Alignment.center,
+        width: double.infinity,
+        height: 56.sp,
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.sp),
+            side: const BorderSide(),
+          ),
         ),
-      ),
-      child: Text(
-        text,
-        style: const TextStyle(
-          color: kMainColor,
-          fontWeight: FontWeight.w800,
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: kMainColor,
+            fontWeight: FontWeight.w800,
+          ),
         ),
       ),
     );
--- lib/screens/onboard_screen.dart
+++ lib/screens/onboard_screen.dart
@@ -1,10 +1,81 @@
+import 'package:biblio/components/custom_button.dart';
+import 'package:biblio/components/height.dart';
+import 'package:biblio/components/width.dart';
+import 'package:biblio/constants/colors_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnboardScreen extends StatelessWidget {
   const OnboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        spacing: 12.sp,
+        children: [
+          SizedBox(
+            height: 410.sp,
+            child: Image.asset(
+              'assets/images/onboared_view.png',
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'اقرأ وبدِّل',
+                style: TextStyle(
+                  color: kMainColor,
+                  fontSize: 24.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const W(w: 10),
+              SvgPicture.asset(
+                'assets/svg/logo.svg',
+                height: 20.sp,
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.sp),
+            child: Text(
+              'هل تحب القراءة وترغب في مشاركة مكتبتك مع الآخرين؟ تطبيق Biblio يمنحك فرصة لعرض الكتب التي ترغب في تبادلها أو بيعها، واكتشاف كنوزًا جديدة في مكتبات القراء من حولك.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: kTextColor,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w500,
+                height: 1.5.sp,
+              ),
+            ),
+          ),
+          const H(h: 4),
+          const CustomButton(
+            text: 'تسجيل الدخول',
+          ),
+          const CustomBorderBotton(
+            text: 'حساب جديد',
+          ),
+          Text(
+            'الدخول كزائر',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: const Color(0xFF3E5879),
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w800,
+              decoration: TextDecoration.underline,
+              decorationColor: kMainColor,
+              height: 1.35.sp,
+            ),
+          ),
+          // width: MediaQuery.of(context).size.width,
+        ],
+      ),
+    );
   }
 }
