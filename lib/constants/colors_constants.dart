8769023703c38aceacaf93e9816d8cf34a7f4bfb
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-01-04 07:14:34 +0200

    Add SVG asset for books icon and update color constants
    Added a new SVG asset for the books icon and created a new file for color constants to standardize color usage across the application. Also, updated the `pubspec.yaml` to include the new fonts directory and assets.

---
+++ lib/constants/colors_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kMainColor = Color(0xFF213555);
+const kScaffoldBackgroundColor = Color(0xFFFBFBFB);
+const kTextColor = Color(0xFF686868);
+const kTextShadowColor=Color(0xFFB1B1B1);
+const kHeader1Color = Color(0xFF3A3A3A);
+const kBorderColor = Color(0xFFD3D3D3);
+const kDisableButtonColor = Color(0xFFEFEFEF);
