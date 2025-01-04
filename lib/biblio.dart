e7cca70d5344f8a70e7108ebfc2ea335eb60b1cc
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-01-04 22:22:25 +0200

    Add Arabic and English translation support using easy_localization
    Implemented easy_localization package for handling translations in Arabic and English. Added initial empty JSON files for translations and configured the main application to support localization.

--- lib/biblio.dart
+++ lib/biblio.dart
@@ -3,6 +3,7 @@ import 'package:biblio/screens/home_page_screen.dart';
 import 'package:biblio/screens/login/login_screen.dart';
 import 'package:biblio/screens/onboard_screen.dart';
 import 'package:biblio/screens/splash_screen.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -11,14 +12,24 @@ class Biblio extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    /// ScreenUtils
     return ScreenUtilInit(
       designSize: const Size(
         390,
         844,
       ),
       minTextAdapt: true,
+
+      /// MaterialApp
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
+
+        /// Localizations
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+
+        /// Theme
         theme: ThemeData(
           scaffoldBackgroundColor: kScaffoldBackgroundColor,
           textTheme: Theme.of(
@@ -27,6 +38,8 @@ class Biblio extends StatelessWidget {
                 fontFamily: 'Avenir Arabic',
               ),
         ),
+
+        /// Routes
         routes: {
           SplashScreen.id: (context) => const SplashScreen(),
           OnboardScreen.id: (context) => const OnboardScreen(),
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:biblio/biblio.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
   runApp(
-    const Biblio(),
+    EasyLocalization(
+      supportedLocales: const [
+        Locale('ar'),
+        Locale('en'),
+      ],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('ar'),
+      startLocale: const Locale('ar'),
+      child: const Biblio(),
+    ),
   );
 }
