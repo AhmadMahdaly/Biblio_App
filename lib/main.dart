cadcb590ae1d671a02ca037aff09bd25ab53590a
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-01-06 11:52:36 +0200

    Add Firebase dependencies and configuration
    - Updated `build.gradle` to include Google Services plugin.
    - Modified `defaultConfig` in `build.gradle` to set the correct `applicationId` and `minSdkVersion`.
    - Added `google-services.json` for Firebase configuration.
    - Updated `main.dart` to initialize Firebase with platform-specific options.
    - Registered Firebase plugins in `GeneratedPluginRegistrant.swift` for macOS.
    - Added Firebase dependencies to `pubspec.yaml` and `pubspec.lock`.
    - Registered Firebase plugins in `generated_plugin_registrant.cc` and `generated_plugins.cmake` for Windows.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
+import 'dart:io';
+
 import 'package:biblio/biblio.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await EasyLocalization.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+            apiKey: 'AIzaSyC9i6RaQ2wVSLEIrN4qv4tEGPcQXsFV03Y',
+            appId: '1:157744080685:android:d69e8f360d472dac6c55a4',
+            messagingSenderId: '157744080685',
+            projectId: 'biblio-5960c',
+          ),
+        )
+      : await Firebase.initializeApp();
 
   runApp(
     EasyLocalization(
