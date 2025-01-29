0ef71b130add22dc5b30474b380aa6b324e3a264
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-01-29 18:53:06 +0200

    Refactor conversation card and add conversation room
    Updated the MessageCard widget to remove unnecessary dependencies and streamline the conversation display.
    Introduced a new ConversationRoom widget to handle message fetching and sending, improving code organization and separation of concerns.

--- lib/screens/orders_page/widgets/conversation_card.dart
+++ lib/screens/orders_page/widgets/conversation_card.dart
@@ -1,21 +1,17 @@
-import 'package:biblio/cubit/messages/fetch_messages_cubit/fetch_messages_cubit.dart';
 import 'package:biblio/screens/orders_page/widgets/conversation_room.dart';
-import 'package:biblio/utils/components/app_indicator.dart';
-import 'package:biblio/utils/components/show_snackbar.dart';
+import 'package:biblio/utils/components/height.dart';
 import 'package:biblio/utils/constants/colors_constants.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MessageCard extends StatefulWidget {
   const MessageCard({
     required this.conversation,
-    required this.conversationId,
     super.key,
   });
 
   final Map<String, dynamic> conversation;
-  final String conversationId;
 
   @override
   State<MessageCard> createState() => _MessageCardState();
@@ -25,109 +21,98 @@ class _MessageCardState extends State<MessageCard> {
   @override
   void initState() {
     super.initState();
-    context
-        .read<FetchMessagesCubit>()
-        .fetchMessages(conversationId: widget.conversationId);
+    // context.read<FetchMessagesCubit>().fetchMessages(
+    //       conversationId: widget.conversation['conversation_id'].toString(),
+    //     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<FetchMessagesCubit, FetchMessagesState>(
-      listener: (context, state) {
-        if (state is FetchMessagesError) {
-          showSnackBar(context, state.message);
-        }
-      },
-      builder: (context, state) {
-        final fetchMessageCubit = context.read<FetchMessagesCubit>();
-        return state is FetchMessagesLoading
-            ? const AppIndicator()
-            : InkWell(
-                onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ConversationRoom(
-                      conversationId: widget.conversationId,
-                      titleBook: fetchMessageCubit.titleBook.toString(),
+    return
+        //  Container(
+        //             margin: EdgeInsets.symmetric(
+        //               horizontal: 16.sp,
+        //               vertical: 5.sp,
+        //             ),
+        //             decoration: BoxDecoration(
+        //               color: const Color(0xFFF7F7F7),
+        //               borderRadius: BorderRadius.circular(10.sp),
+        //             ),
+        //             height: 72.sp,
+        //             child: const AppIndicator(size: 10),
+        //           )
+        //         :
+        InkWell(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ConversationRoom(
+            conversationId: widget.conversation['conversation_id'].toString(),
+            titleBook: widget.conversation['title_book'].toString(),
+          ),
+        ),
+      ),
+      child: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: 16.sp,
+          vertical: 5.sp,
+        ),
+        decoration: BoxDecoration(
+          color: const Color(0xFFF7F7F7),
+          borderRadius: BorderRadius.circular(10.sp),
+        ),
+        height: 72.sp,
+        child: Row(
+          spacing: 16.sp,
+          children: [
+            Container(
+              height: 60.sp,
+              width: 60.sp,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(320),
+              ),
+              child: CachedNetworkImage(
+                imageUrl: widget.conversation['book_image'].toString(),
+                fit: BoxFit.cover,
+                height: 60.sp,
+                width: 60.sp,
+                errorListener: (_) => Icon(
+                  Icons.account_circle,
+                  size: 60.sp,
+                  color: kMainColor,
+                ),
+                errorWidget: (context, url, error) => Icon(
+                  Icons.account_circle,
+                  size: 60.sp,
+                  color: kMainColor,
+                ),
+              ),
+            ),
+            Column(
+              spacing: 10.sp,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const H(h: 6),
+                SizedBox(
+                  width: 200.sp,
+                  child: Text(
+                    'طلب كتاب: ${widget.conversation['title_book']}',
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: kMainColor,
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w800,
+                      height: 1.70,
+                      letterSpacing: 0.10,
                     ),
                   ),
                 ),
-                child: Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 16.sp,
-                    vertical: 5.sp,
-                  ),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFF7F7F7),
-                    borderRadius: BorderRadius.circular(10.sp),
-                  ),
-                  height: 72.sp,
-                  child: Row(
-                    spacing: 16.sp,
-                    children: [
-                      Container(
-                        height: 60.sp,
-                        width: 60.sp,
-                        clipBehavior: Clip.antiAlias,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(320),
-                        ),
-                        child: Image.network(
-                          fetchMessageCubit.bookImg.toString(),
-                          //   ['book_image'].toString(),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Column(
-                        spacing: 10.sp,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            width: 200.sp,
-                            child: Text(
-                              'طلب كتاب: ${fetchMessageCubit.titleBook}',
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                color: kMainColor,
-                                fontSize: 14.sp,
-                                fontWeight: FontWeight.w800,
-                                height: 1.70,
-                                letterSpacing: 0.10,
-                              ),
-                            ),
-                          ),
-                          // SizedBox(
-                          //   width: 240.sp,
-                          //   child: Text(
-                          //     '${fetchMessageCubit.messages[''].toString()} ',
-                          //     overflow: TextOverflow.ellipsis,
-                          //     maxLines: 2,
-                          //     style: TextStyle(
-                          //       color: kTextColor,
-                          //       fontSize: 12.sp,
-                          //       fontWeight: FontWeight.w500,
-                          //       height: 1.35,
-                          //     ),
-                          //   ),
-                          // ),
-                        ],
-                      ),
-                      // SizedBox(
-                      //   child: Text(
-                      //     message['created_at'].toString(),
-                      //     overflow: TextOverflow.ellipsis,
-                      //     style: TextStyle(
-                      //       color: kTextColor,
-                      //       fontSize: 12.sp,
-                      //       fontWeight: FontWeight.w500,
-                      //     ),
-                      //   ),
-                      // ),
-                    ],
-                  ),
-                ),
-              );
-      },
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
