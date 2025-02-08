0702acfa83b2fa083ce5597f9c2067709339bc43
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-02-08 08:30:55 +0200

    Refactor (home page) layout and improve spacing:
    Adjust the layout of the home page by modifying the padding and spacing of various widgets.
    Update the search bar height and add additional spacing to improve the overall appearance.
    Ensure that the category list view and new books list view maintain consistent dimensions for a better user experience.

--- lib/screens/home_page/home_page_screen.dart
+++ lib/screens/home_page/home_page_screen.dart
@@ -3,8 +3,8 @@ import 'package:biblio/screens/category_page/widgets/category_listview.dart';
 import 'package:biblio/screens/category_page/widgets/see_all.dart';
 import 'package:biblio/screens/home_page/widgets/home_banner.dart';
 import 'package:biblio/screens/home_page/widgets/new_books_listview.dart';
-import 'package:biblio/screens/search/home_search_textfield.dart';
 import 'package:biblio/screens/home_page/widgets/title_header_home.dart';
+import 'package:biblio/screens/search/home_search_textfield.dart';
 import 'package:biblio/utils/components/height.dart';
 import 'package:biblio/utils/components/width.dart';
 import 'package:biblio/utils/constants/colors_constants.dart';
@@ -28,120 +28,119 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: RefreshIndicator(
-        strokeWidth: 0.9,
-        color: kMainColor,
-        onRefresh: fetchDate,
-        child: Scaffold(
-          body: CustomScrollView(
-            slivers: [
-              /// Search bar
-              SliverAppBar(
-                toolbarHeight: 80.sp,
-                automaticallyImplyLeading: false,
-                title: const Column(
+    return RefreshIndicator(
+      strokeWidth: 0.9,
+      color: kMainColor,
+      onRefresh: fetchDate,
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            /// Search bar
+            SliverAppBar(
+              toolbarHeight: 80.sp,
+              automaticallyImplyLeading: false,
+              title: const Column(
+                children: [
+                  H(h: 12),
+                  HomeSearchTextfield(),
+                  H(h: 6),
+                ],
+              ),
+            ),
+
+            /// Home Banner
+            const SliverToBoxAdapter(
+              child: HomeBanner(),
+            ),
+
+            /// Books Categories
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.sp),
+                child: Row(
                   children: [
-                    H(h: 10),
-                    HomeSearchTextfield(),
+                    const TitleHeaderHome(
+                      text: 'فئات الكتب',
+                    ),
+                    const Spacer(),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return const CategorySeeAll();
+                            },
+                          ),
+                        );
+                      },
+                      child: Text(
+                        'عرض الكل',
+                        style: TextStyle(
+                          color: const Color(0xFFA4CFC3),
+                          fontSize: 13.sp,
+                          fontWeight: FontWeight.w800,
+                          decoration: TextDecoration.underline,
+                          decorationColor: const Color(0xFFA4CFC3),
+                        ),
+                      ),
+                    ),
+                    const W(w: 16),
                   ],
                 ),
               ),
+            ),
 
-              /// Home Banner
-              const SliverToBoxAdapter(
-                child: HomeBanner(),
+            /// Categories ListView
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 110.sp,
+                width: 85.sp,
+                child: const CategoryListview(),
               ),
+            ),
 
-              /// Books Categories
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.sp),
-                  child: Row(
-                    children: [
-                      const TitleHeaderHome(
-                        text: 'فئات الكتب',
+            /// New books title
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  right: 16.sp,
+                  left: 16.sp,
+                  top: 16.sp,
+                  bottom: 12.sp,
+                ),
+                child: Row(
+                  spacing: 10.sp,
+                  children: [
+                    Text(
+                      'أحدث الكتب',
+                      style: TextStyle(
+                        color: kMainColor,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w800,
                       ),
-                      const Spacer(),
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return const CategorySeeAll();
-                              },
-                            ),
-                          );
-                        },
-                        child: Text(
-                          'عرض الكل',
-                          style: TextStyle(
-                            color: const Color(0xFFA4CFC3),
-                            fontSize: 13.sp,
-                            fontWeight: FontWeight.w800,
-                            decoration: TextDecoration.underline,
-                            decorationColor: const Color(0xFFA4CFC3),
-                          ),
-                        ),
-                      ),
-                      const W(w: 16),
-                    ],
-                  ),
+                    ),
+                    SvgPicture.asset(
+                      'assets/svg/logo.svg',
+                      height: 16.sp,
+                    ),
+                  ],
                 ),
               ),
+            ),
 
-              /// Categories ListView
-              SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 110.sp,
-                  width: 80.sp,
-                  child: const CategoryListview(),
-                ),
+            /// New Books ListView
+            SliverToBoxAdapter(
+              child: SizedBox(
+                width: 140.sp,
+                height: 280.sp,
+                child: const NewBooksListview(),
               ),
-
-              /// New books title
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    right: 16.sp,
-                    left: 16.sp,
-                    top: 16.sp,
-                    bottom: 12.sp,
-                  ),
-                  child: Row(
-                    spacing: 10.sp,
-                    children: [
-                      Text(
-                        'أحدث الكتب',
-                        style: TextStyle(
-                          color: kMainColor,
-                          fontSize: 18.sp,
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                      SvgPicture.asset(
-                        'assets/svg/logo.svg',
-                        height: 18.5.sp,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-
-              /// New Books ListView
-              SliverToBoxAdapter(
-                child: SizedBox(
-                  width: 140.sp,
-                  height: 280.sp,
-                  child: const NewBooksListview(),
-                ),
-              ),
-              const SliverToBoxAdapter(
-                child: H(h: 16),
-              ),
-            ],
-          ),
+            ),
+            const SliverToBoxAdapter(
+              child: H(h: 16),
+            ),
+          ],
         ),
       ),
     );
