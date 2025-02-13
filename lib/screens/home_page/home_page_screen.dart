492e0a1df9a0ff20d03eac6f3984708f79869a6c
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-02-13 16:16:08 +0200

    Add conversation cubit to fetch unread messages
    Updated the fetchDate method in HomePage to include the CreateConversationCubit.
    This allows fetching unread messages with the context and the other user's ID, enhancing the functionality of the home page.

--- lib/screens/home_page/home_page_screen.dart
+++ lib/screens/home_page/home_page_screen.dart
@@ -1,4 +1,5 @@
 import 'package:biblio/cubit/books/fetch_located_books_cubit/fetch_located_books_cubit.dart';
+import 'package:biblio/cubit/messages/create_conversation_cubit/create_conversation_cubit.dart';
 import 'package:biblio/cubit/messages/fetch_unread_message_cubit/fetch_unread_message_cubit.dart';
 import 'package:biblio/screens/category_page/widgets/category_listview.dart';
 import 'package:biblio/screens/category_page/widgets/see_all.dart';
@@ -25,7 +26,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Future<void> fetchDate() async {
     await context.read<FetchLocatedBooksCubit>().fetchLocatedBooks(context);
-    await context.read<FetchUnreadMessageCubit>().fetchUnreadMessages();
+    await context.read<FetchUnreadMessageCubit>().fetchUnreadMessages(
+          context,
+          otherId:
+              context.read<CreateConversationCubit>().otherUserId.toString(),
+        );
   }
 
   @override
