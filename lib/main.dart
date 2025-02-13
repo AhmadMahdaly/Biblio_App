f796fd84a798a0d3e736253f64049ad823db18e2
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-02-13 16:06:34 +0200

    Update version and comment out Firebase integration
    This commit updates the application version to 1.0.3+4 in the pubspec.yaml file.
    Additionally, it comments out the Firebase initialization and messaging code in main.dart, which is not currently in use.
    This prepares the codebase for potential future integration while keeping the current functionality intact.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+// import 'dart:io';
+
 import 'package:biblio/booklink.dart';
 import 'package:biblio/utils/controller/connectivity_controller.dart';
 import 'package:easy_localization/easy_localization.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
+// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+//   // هذا الكود يعمل عند وصول إشعار أثناء إغلاق التطبيق
+//   print("رسالة جديدة: ${message.messageId}");
+// }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,6 +36,7 @@ void main() async {
   //         ),
   //       )
   //     : await Firebase.initializeApp();
+  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
 // initialize supabase
   final supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
@@ -55,4 +65,4 @@ void main() async {
   );
 }
 
-/// shorebird patch --platforms=android --release-version=1.0.1+2
+/// shorebird patch --platforms=android --release-version=1.0.3+4
